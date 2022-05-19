a1c06e01e71dbd6d0ecc053a03a90e9932491ef2
JianFengH <707156268@qq.com>
2022-05-19 22:02:10 +0800

    Use never

--- src/hello.ts
+++ src/hello.ts
@@ -3,3 +3,75 @@ function greet(person: string, date: Date) {
 }
  
 greet("Maddison", new Date());
+
+const constString = "hello";
+
+function doSomething(x: string | null) {
+  if (x === null) {
+    // do nothing
+  } else {
+    console.log("Hello, " + x.toUpperCase());
+  }
+}
+
+function liveDangerously(x?: number | null) {
+  // No error
+  console.log(x!.toFixed());
+}
+
+function printAll(strs: string | string[] | null) {
+  if (strs && typeof strs === "object") {
+    for (const s of strs) {
+      console.log(s);
+    }
+  } else if (typeof strs === "string") {
+    console.log(strs);
+  } else {
+    // do nothing
+  }
+}
+
+interface Container {
+  value: number | null | undefined;
+}
+ 
+function multiplyValue(container: Container, factor: number) {
+  // Remove both 'null' and 'undefined' from the type.
+  if (container.value != null) { // null == undefined
+    console.log(container.value);
+                           
+    // Now we can safely multiply 'container.value'.
+    container.value *= factor;
+  }
+}
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+ 
+interface Square {
+  kind: "square";
+  sideLength: number;
+}
+ 
+interface Triangle {
+  kind: "triangle";
+  sideLength: number;
+}
+ 
+type Shape = Circle | Square | Triangle;
+ 
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.sideLength ** 2;
+    case "triangle":
+      return shape.sideLength * shape.sideLength / 2;
+    default:
+      const _exhaustiveCheck: never = shape;
+      return _exhaustiveCheck;
+  }
+}
