17e5542b747a068480a360b6d7578ef848ba594b
JianFengH <707156268@qq.com>
2022-05-19 22:36:39 +0800

    Use call signature & generics

--- src/hello.ts
+++ src/hello.ts
@@ -75,3 +75,59 @@ function getArea(shape: Shape) {
       return _exhaustiveCheck;
   }
 }
+
+type GreetFunction = (s: string) => void;
+
+function greeter(fn: (a: string) => void) {
+  fn("Hello, World");
+}
+ 
+function printToConsole(s: string) {
+  console.log(s);
+}
+ 
+greeter(printToConsole);
+
+// Call Signatures
+type DescribableFunction = {
+  description: string;
+  (someArg: number): boolean;
+};
+function doSomething2(fn: DescribableFunction) {
+  console.log(fn.description + " returned " + fn(6));
+}
+
+// Construct Signatures
+type SomeConstructor = {
+  new (s: string): Date;
+};
+function fn(ctor: SomeConstructor) {
+  return new ctor("hello");
+}
+
+// Some objects, like JavaScript’s Date object, can be called with or without new. 
+// You can combine call and construct signatures in the same type arbitrarily:
+interface CallOrConstruct {
+  new (s: string): Date;
+  (n?: number): number;
+}
+
+// Generic Functions
+function firstElement<Type>(arr: Type[]): Type | undefined {
+  return arr[0];
+}
+// s is of type 'string'
+const s = firstElement(["a", "b", "c"]);
+// n is of type 'number'
+const n = firstElement([1, 2, 3]);
+// u is of type undefined
+const u = firstElement([]);
+
+// Generic inference
+function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
+  return arr.map(func);
+}
+ 
+// Parameter 'n' is of type 'string'
+// 'parsed' is of type 'number[]'
+const parsed = map(["1", "2", "3"], (n) => parseInt(n));
